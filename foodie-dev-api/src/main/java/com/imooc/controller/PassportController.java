ede7fd92ce9cdb8f7b80e3232088938b137c4d87
zhoujiashen <282581063@qq.com>
2020-01-05 15:46:35 +0800

    用户注册

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,13 +1,17 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import com.imooc.utils.CookieUtils;
 import com.imooc.utils.JSONResult;
+import com.imooc.utils.JsonUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @RestController
@@ -34,6 +38,63 @@ public class PassportController {
 
         // 3. 请求成功，用户名没有重复
         return JSONResult.ok();
+    }
+
+
+    @PostMapping("/regist")
+    public JSONResult regist(@RequestBody UserBO userBO,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPwd = userBO.getConfirmPassword();
+
+        // 0. 判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password) ||
+                StringUtils.isBlank(confirmPwd)) {
+            return JSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        // 1. 查询用户名是否存在
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return JSONResult.errorMsg("用户名已经存在");
+        }
+
+        // 2. 密码长度不能少于6位
+        if (password.length() < 6) {
+            return JSONResult.errorMsg("密码长度不能少于6");
+        }
+
+        // 3. 判断两次密码是否一致
+        if (!password.equals(confirmPwd)) {
+            return JSONResult.errorMsg("两次密码输入不一致");
+        }
+
+        // 4. 实现注册
+        Users userResult = userService.createUser(userBO);
+
+//        userResult = setNullProperty(userResult);
+//
+//        CookieUtils.setCookie(request, response, "user",
+//                JsonUtils.objectToJson(userResult), true);
+
+        // TODO 生成用户token，存入redis会话
+        // TODO 同步购物车数据
+
+        return JSONResult.ok();
+    }
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setPassword(null);
+        userResult.setMobile(null);
+        userResult.setEmail(null);
+        userResult.setCreatedTime(null);
+        userResult.setUpdatedTime(null);
+        userResult.setBirthday(null);
+        return userResult;
     }
 
 
--- foodie-dev-common/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
+++ foodie-dev-common/foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -1,6 +1,7 @@
 package com.imooc.service;
 
 import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
 
 
 public interface UserService {
@@ -9,5 +10,10 @@ public interface UserService {
      * 判断用户名是否存在
      */
     public boolean queryUsernameIsExist(String username);
+
+    /**
+     * 判断用户名是否存在
+     */
+    public Users createUser(UserBO userBO);
 
 }
