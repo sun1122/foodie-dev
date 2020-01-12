3885513ff9ce432f705a3db011ebdc79e291fea1
zhoujiashen <282581063@qq.com>
2020-01-12 23:42:36 +0800

    首页轮播图显示

---
+++ foodie-dev-common/src/main/java/com/imooc/enums/YesOrNo.java
@@ -0,0 +1,17 @@
+package com.imooc.enums;
+
+/**
+ * @Desc: 是否 枚举
+ */
+public enum YesOrNo {
+    YES(1, "是"),
+    NO(0, "否");
+
+    public final Integer type;
+    public final String value;
+
+    YesOrNo(Integer type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
