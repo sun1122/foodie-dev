604aa6766270a2c98c8956d6a0c54ed54bccdbef
zhoujiashen <282581063@qq.com>
2020-01-13 23:58:25 +0800

    首页查询一级分类以及懒加载子分类

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -2,12 +2,17 @@ package com.imooc.controller;
 
 import com.imooc.enums.YesOrNo;
 import com.imooc.pojo.Carousel;
+import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 import com.imooc.service.CarouselService;
+import com.imooc.service.CategoryService;
 import com.imooc.utils.JSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +25,8 @@ public class IndexController {
 
     @Autowired
     private CarouselService carouselService;
-
+    @Autowired
+    private CategoryService categoryService;
 
     @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
     @GetMapping("/carousel")
@@ -29,5 +35,29 @@ public class IndexController {
         return JSONResult.ok(list);
     }
 
+    /**
+     * 首页分类展示需求：
+     * 1. 第一次刷新主页查询大分类，渲染展示到首页
+     * 2. 如果鼠标上移到大分类，则加载其子分类的内容，如果已经存在子分类，则不需要加载（懒加载）
+     */
+    @ApiOperation(value = "获取商品分类(一级分类)", notes = "获取商品分类(一级分类)", httpMethod = "GET")
+    @GetMapping("/cats")
+    public JSONResult cats() {
+        List<Category> list = categoryService.queryAllRootLevelCat();
+        return JSONResult.ok(list);
+    }
 
+    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
+    @GetMapping("/subCat/{rootCatId}")
+    public JSONResult subCat(
+            @ApiParam(name = "rootCatId", value = "一级分类id", required = true)
+            @PathVariable Integer rootCatId) {
+
+        if (rootCatId == null) {
+            return JSONResult.errorMsg("分类不存在");
+        }
+
+        List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
+        return JSONResult.ok(list);
+    }
 }
