b9d9c6cf638d6b677bac10516d67819f63da3b82
zhoujiashen <282581063@qq.com>
2020-02-04 17:01:37 +0800

    商品详情页模块开发

---
+++ foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -0,0 +1,41 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @param itemId
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id查询商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+
+}
