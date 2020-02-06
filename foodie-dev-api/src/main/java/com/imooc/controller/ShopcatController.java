4879bfc425d38b5543a193162f09481fa76c7bed
zhoujiashen <282581063@qq.com>
2020-02-06 17:08:50 +0800

    商品购物车模块：加入购物车

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
@@ -0,0 +1,40 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.bo.ShopcartBO;
+import com.imooc.utils.JSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Api(value = "购物车接口controller", tags = {"购物车接口相关的api"})
+@RequestMapping("shopcart")
+@RestController
+public class ShopcatController {
+
+    @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车", httpMethod = "POST")
+    @PostMapping("/add")
+    public JSONResult add(
+            @RequestParam String userId,
+            @RequestBody ShopcartBO shopcartBO,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        if (StringUtils.isBlank(userId)) {
+            return JSONResult.errorMsg("");
+        }
+
+        System.out.println(shopcartBO);
+
+        // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+
+        return JSONResult.ok();
+    }
+
+
+
+}
